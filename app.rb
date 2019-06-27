df7dff3b8cd2a590957300b4b9c099eb56a9ca14
Jhana Gustave <jhana.gustave@gmail.com>
2019-06-27 16:33:05 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,4 +6,13 @@ get '/' do
   erb :index
 end
 
+get '/new' do
+    erb :create_puppy
+  end
+
+  post '/' do
+    @puppy = Puppy.new(params[:name], params[:breed], params[:age])
+    erb :display_puppy
+  end
+
 end
