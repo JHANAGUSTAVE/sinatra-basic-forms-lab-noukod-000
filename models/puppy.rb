880ef7f011e7dda8088f25455b52a07bbb00d18d
Jhana Gustave <jhana.gustave@gmail.com>
2019-06-27 16:17:34 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -2,8 +2,8 @@ class Puppy
   attr_accessor :name, :breed, :age
 
   def initialize(@age, @name, @breed)
-    @name = name 
-    @breed = breed 
+    @name = name
+    @breed = breed
     @age = age
   end
 end
